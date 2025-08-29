aea9e0ce2c4fcff943fe075fd5e9f52703f53195
Erica Ap <erica6268@gmail.com>
2025-08-29 15:12:11 -0300

    exercicio extra 2

--- cypress/e2e/duplication/sample2.cy.js
+++ cypress/e2e/duplication/sample2.cy.js
@@ -15,28 +15,35 @@ describe('Code duplication bad practice - repetitive tests', () => {
       .clear()
   })
 
-  it('searches for "reactjs"', () => {
-    // cy.get('@searchField')
-    //   .type('reactjs{enter}')
+  const termsToSearchForm = ['reacjs', 'vue.js', 'angular']
 
-    cy.search('reactjs{enter}')
-
-    cy.wait('@getStories')
-
-    cy.get('.table-row')
-      .should('have.length', 100)
+  termsToSearchForm.forEach(term => {
+    it(`searches for "${term}"`, () => { 
+      cy.search(term)
+  
+      cy.wait('@getStories')
+  
+      cy.get('.table-row')
+        .should('have.length', 100)
+    })
   })
+  // it('searches for "reactjs"', () => {
+  //   cy.search('reactjs{enter}')
 
-  it('searches for "vuejs"', () => {
-    // cy.get('@searchField')
-    //   .type('vuejs{enter}')
+  //   cy.wait('@getStories')
 
-    cy.search('vuejs{enter}')
-    cy.wait('@getStories')
+  //   cy.get('.table-row')
+  //     .should('have.length', 100)
+  // })
 
-    cy.get('.table-row')
-      .should('have.length', 100)
-  })
+  // it('searches for "vuejs"', () => {
+  //   cy.search('vuejs{enter}')
+  //   cy.wait('@getStories')
+
+  //   cy.get('.table-row')
+  //     .should('have.length', 100)
+  // })
+
 })
 
 
